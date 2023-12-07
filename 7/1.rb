df2eec19d066c087cdb6f0c4a39094b3f8bf1bbc
Anders Alfredsson <anders.alfredsson@devies.se>
2023-12-07 07:38:50 +0100

    Day 7

---
+++ 7/1.rb
@@ -0,0 +1,64 @@
+require 'set'
+
+class Hand
+    CARDS = %w(2 3 4 5 6 7 8 9 T J Q K A)
+
+    HANDS = %i(five four house three two pair high)
+
+    attr_reader :hand, :bid
+
+    def initialize(hand, bid)
+        @hand = hand.chars
+        @bid = bid.to_i
+    end
+
+    def type
+        combined = :high
+        Set.new(@hand).each do |c|
+            case @hand.count(c)
+            when 5
+                return :five
+            when 4
+                return :four
+            when 3
+                if combined == :pair
+                    return :house
+                end
+                combined = :three
+            when 2
+                if combined == :three
+                    return :house
+                elsif combined == :pair
+                    return :two
+                end
+                combined = :pair
+            end
+        end
+        combined
+    end
+
+    def <=>(other)
+        ts, to = HANDS.index(type), HANDS.index(other.type)
+        return ts <=> to unless ts == to
+
+        5.times do |i|
+            cs, co = CARDS.index(@hand[i]), CARDS.index(other.hand[i])
+            return co <=> cs unless cs == co
+        end
+        return 0
+    end
+
+    def to_s
+        "#{@hand.join} #{@bid}"
+    end
+end
+
+hands = File.readlines('input').map { |line| Hand.new(*line.split(' ')) }.sort.reverse
+
+total = 0
+
+hands.each_with_index do |h, i|
+    total += h.bid * (i + 1)
+end
+
+puts total
---
+++ 7/2.rb
@@ -0,0 +1,77 @@
+require 'set'
+
+class Hand
+    CARDS = %w(J 2 3 4 5 6 7 8 9 T Q K A)
+
+    HANDS = %i(five four house three two pair high)
+
+    attr_reader :hand, :bid
+
+    def initialize(hand, bid)
+        @hand = hand.chars
+        @bid = bid.to_i
+    end
+
+    def type
+        return :five if @hand.count('J') == 5
+
+        sorted = @hand.sort do |a, b|
+            ac, bc = @hand.count(a), hand.count(b)
+            if ac == bc
+                CARDS.index(b) <=> CARDS.index(a)
+            else
+                bc <=> ac
+            end
+        end
+
+        combined = :high
+        joker_used = false
+        (Set.new(sorted) - Set['J']).each do |c|
+            case @hand.count(c) + (joker_used ? 0 : @hand.count('J'))
+            when 5
+                return :five
+            when 4
+                return :four
+            when 3
+                if combined == :pair
+                    return :house
+                end
+                combined = :three
+            when 2
+                if combined == :three
+                    return :house
+                elsif combined == :pair
+                    return :two
+                end
+                combined = :pair
+            end
+            joker_used = true
+        end
+        combined
+    end
+
+    def <=>(other)
+        ts, to = HANDS.index(type), HANDS.index(other.type)
+        return ts <=> to unless ts == to
+
+        5.times do |i|
+            cs, co = CARDS.index(@hand[i]), CARDS.index(other.hand[i])
+            return co <=> cs unless cs == co
+        end
+        return 0
+    end
+
+    def to_s
+        "#{@hand.join} (#{type}) #{@bid}"
+    end
+end
+
+hands = File.readlines('input').map { |line| Hand.new(*line.split(' ')) }.sort.reverse
+
+total = 0
+
+hands.each_with_index do |h, i|
+    total += h.bid * (i + 1)
+end
+
+puts total
